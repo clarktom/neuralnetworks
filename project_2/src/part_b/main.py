8620ac41da40aee3c07ced705a8391e5213d5c15
karsa755 <karimvieri@hotmail.com>
2017-11-10 09:56:06 +0800

    some stuff

--- project_2/src/part_b/main.py
+++ project_2/src/part_b/main.py
@@ -61,24 +61,24 @@ b_end = init_bias(10)
 
 print("lmao familicious fam")
 
-tilde_x = theano_rng.binomial(size=x.shape, n=1, p=1 - corruption_level,
-                              dtype=theano.config.floatX)*x
-y1 = T.nnet.sigmoid(T.dot(tilde_x, W1) + b1)
+#tilde_x = theano_rng.binomial(size=x.shape, n=1, p=1 - corruption_level,
+#                              dtype=theano.config.floatX)*x
+y1 = T.nnet.sigmoid(T.dot(x, W1) + b1)
 z1_1 = T.nnet.sigmoid(T.dot(y1, W1_prime) + b1_prime)
 cost1 = - T.mean(T.sum(x * T.log(z1_1) + (1 - x) * T.log(1 - z1_1), axis=1))
 
 yy1 = T.fmatrix('yy1')
-tilde_y1 = theano_rng.binomial(size=yy1.shape, n=1, p=1 - corruption_level,
-                              dtype=theano.config.floatX)*yy1
-y2 = T.nnet.sigmoid(T.dot(tilde_y1, W2) + b2)
+#tilde_y1 = theano_rng.binomial(size=yy1.shape, n=1, p=1 - corruption_level,
+#                              dtype=theano.config.floatX)*yy1
+y2 = T.nnet.sigmoid(T.dot(yy1, W2) + b2)
 z2_2 = T.nnet.sigmoid(T.dot(y2, W2_prime) + b2_prime)
 z1_2 = T.nnet.sigmoid(T.dot(z2_2, W1_prime) + b1_prime)
 cost2 = - T.mean(T.sum(x * T.log(z1_2) + (1 - x) * T.log(1 - z1_2), axis=1))
 
 yy2 = T.fmatrix('yy2')
-tilde_y2 = theano_rng.binomial(size=yy2.shape, n=1, p=1 - corruption_level,
-                              dtype=theano.config.floatX)*yy2
-y3 = T.nnet.sigmoid(T.dot(tilde_y2, W3) + b3)
+#tilde_y2 = theano_rng.binomial(size=yy2.shape, n=1, p=1 - corruption_level,
+#                              dtype=theano.config.floatX)*yy2
+y3 = T.nnet.sigmoid(T.dot(yy2, W3) + b3)
 z3_3 = T.nnet.sigmoid(T.dot(y3, W3_prime) + b3_prime)
 z2_3 = T.nnet.sigmoid(T.dot(z3_3, W2_prime) + b2_prime)
 z1_3 = T.nnet.sigmoid(T.dot(z2_3, W1_prime) + b1_prime)
@@ -120,14 +120,17 @@ test_da3 = theano.function(inputs=[yy2], outputs = [y3, z1_3], updates = None, a
 # train_ffn = theano.function(inputs=[x, d1], outputs = cost2_ffn, updates = updates2_ffn, allow_input_downcast = True)
 # test_ffn = theano.function(inputs=[x], outputs = y2_ffn, allow_input_downcast=True)
 
-
+tilde_trX = T.fmatrix('tilde_trX')
 print('training dae1 ...')
 d1 = []
 for epoch in range(training_epochs):
+
     # go through trainng set
     c = []
     for start, end in zip(range(0, len(trX), batch_size), range(batch_size, len(trX), batch_size)):
-        cost = train_da1(trX[start:end])
+        tilde_trX = theano_rng.binomial(size=trX[start:end].shape, n=1, p=1 - corruption_level,
+                                     dtype=theano.config.floatX)*trX[start:end]
+        cost = train_da1(tilde_trX[start:end])
         c.append(cost)
     d1.append(np.mean(c, dtype='float64'))
     print(d[epoch])
@@ -205,41 +208,40 @@ pylab.savefig('firstLayerWeights')
 # pylab.savefig('thirdLayerWeights')
 
 #reconstructed images
-tilde_teX = np.zeros(teX[:100].shape)
-print(tilde_teX.shape)
-for i, x in enumerate(teX[:100]):
-    tilde_x = theano_rng.binomial(size=x.shape, n=1, p=1 - corruption_level, dtype=theano.config.floatX)*x
-    tilde_teX[i] = tilde_x # TODO: reformat tilde_x
-# tilde_teX = np.reshape(tilde_teX, teX[:100].shape)
-
-yy1, zz1 = test_da1(tilde_teX)
-yy2, zz2 = test_da2(yy1)
-yy3, zz3 = test_da3(yy2)
-
 pylab.figure()
 pylab.gray()
 for i in range(100):
     pylab.subplot(10, 10, i+1); pylab.axis('off'); pylab.imshow(teX[i,:].reshape(28,28))
 pylab.title('input image first layer')
 pylab.savefig('figure_8.3c_3.png')
-
+yy1, zz1 = test_da1(teX[:100])
+yy2, zz2 = test_da2(yy1)
+yy3, zz3 = test_da3(yy2)
 
 pylab.figure()
 pylab.gray()
 for i in range(100):
-    pylab.subplot(10, 10, i+1); pylab.axis('off'); pylab.imshow(yy1[i,:].reshape(28,28))
+    # x = np.reshape(teX[i], (1, 784))
+    # print("shapes boi: ")
+    # print(x.shape)
+    # print(teX.shape)
+    # tilde_x = theano_rng.binomial(size=x.shape, n=1, p=1 - corruption_level, dtype=theano.config.floatX)*x
+
+    pylab.subplot(10, 10, i+1); pylab.axis('off'); pylab.imshow(zz1[i,:].reshape(28,28))
 pylab.savefig('reconstructed image first layer')
 
 pylab.figure()
 pylab.gray()
 for i in range(100):
-    pylab.subplot(10, 10, i+1); pylab.axis('off'); pylab.imshow(yy2[i,:].reshape(28,28))
+
+    pylab.subplot(10, 10, i+1); pylab.axis('off'); pylab.imshow(zz2[i,:].reshape(28,28))
 pylab.savefig('reconstructed image second layer')
 
 pylab.figure()
 pylab.gray()
 for i in range(100):
-    pylab.subplot(10, 10, i+1); pylab.axis('off'); pylab.imshow(yy3[i,:].reshape(28,28))
+
+    pylab.subplot(10, 10, i+1); pylab.axis('off'); pylab.imshow(zz3[i,:].reshape(28,28))
 pylab.savefig('reconstructed image third layer')
 
 pylab.show()
